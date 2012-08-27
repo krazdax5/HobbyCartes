ea3c265642eb3b8f87047cb4ad1d6cb9409265f7
Charles Levesque <dreamtooloverture@gmail.com>
2012-08-27 15:49:53 -0400

    Ajoute un doc avec la liste des pages + maquette du site

---
+++ contenu/HobbyCartes/HobbyCartes/HobbyCartes.Master.designer.vb
@@ -0,0 +1,69 @@
+﻿'------------------------------------------------------------------------------
+' <généré automatiquement>
+'     Ce code a été généré par un outil.
+'
+'     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+'     le code est régénéré.
+' </généré automatiquement>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class HobbyCartes
+
+    '''<summary>
+    '''Contrôle head.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents head As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''Contrôle form1.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''Contrôle cphEntete.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents cphEntete As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''Contrôle cphMenu.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents cphMenu As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''Contrôle cphCorps.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents cphCorps As Global.System.Web.UI.WebControls.ContentPlaceHolder
+
+    '''<summary>
+    '''Contrôle cphPiedpage.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents cphPiedpage As Global.System.Web.UI.WebControls.ContentPlaceHolder
+End Class
---
+++ contenu/HobbyCartes/HobbyCartes/pagetest.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/HobbyCartes.Master" CodeBehind="pagetest.aspx.vb" Inherits="HobbyCartes.pagetest" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cphEntete" runat="server">
+    enTeyte
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="cphMenu" runat="server">
+    menu
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="cphCorps" runat="server">
+<h1>Welcome to your website</h1>
+          <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under the Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). If you wish to remove the 'ARaynorDesign' link in the footer of the template, please contact me first, but other than that...</p>
+          <h3>Place your heading here</h3>
+          <div style="width:200px; float:left; padding: 0px 20px 10px 0px;"><img alt="image" src="images/image.jpg" /></div>
+          <p>This website template uses the fancybox jquery tool to enhance the website, click on the image to the right to see. </p>
+          <p>Ut tincidunt, ante vel fermentum iaculis, turpis sem pulvinar diam, sit amet ullamcorper nibh dui ac nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos:</p>
+          <p>Vestibulum tempus urna vitae neque vehicula sit amet tristique felis ultrices. Phasellus eu laoreet mauris. Integer sit amet ante nec ipsum euismod hendrerit et eget sapien. Duis velit ante, semper nec dapibus adipiscing, pellentesque vitae orci. Etiam adipiscing, justo ut faucibus placerat, neque libero accumsan ipsum, non pellentesque ligula nibh id justo. Aenean tellus nisl, bibendum vitae sollicitudin id, faucibus ut mi.</p>
+          <p>Vestibulum tempus urna vitae neque vehicula sit amet tristique felis ultrices. Phasellus eu laoreet mauris. Integer sit amet ante nec ipsum euismod hendrerit et eget sapien. Duis velit ante</p>
+</asp:Content>
+
+<asp:Content ID="Content5" ContentPlaceHolderID="cphPiedpage" runat="server">
+    pieyDePage
+</asp:Content>
