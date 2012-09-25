30bca29744deecdde00ba60b71163d65e55b8ceb
Loïc Vial <contact.coyl@gmail.com>
2012-09-25 12:07:17 -0400

    Suite precedent commit

--- contenu/HobbyCartes/HobbyCartes/MembreEnvoiMessage.aspx.vb
+++ contenu/HobbyCartes/HobbyCartes/MembreEnvoiMessage.aspx.vb
@@ -1,6 +1,8 @@
 ﻿Public Class MembreEnvoiMessage
     Inherits System.Web.UI.Page
 
+    Private membre As Entitees.Membre
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
