a2a9b904e0e3b8da05f2e2a4c59e5191942611d2
Loïc Vial <contact.coyl@gmail.com>
2012-10-01 14:48:27 -0400

    Modif entitee Membre

--- contenu/HobbyCartes/HobbyCartes/Membre.vb
+++ contenu/HobbyCartes/HobbyCartes/Membre.vb
@@ -5,58 +5,90 @@ Namespace Entitees
 
     Public Class Membre
 
-        Private prenom As String
+        Private m_prenom As String
 
-        Private nom As String
+        Private m_nom As String
 
-        Private nomUtilisateur As String
+        Private m_nomUtilisateur As String
 
-        Private motDePasse As String
+        Private m_motDePasse As String
 
-        Private ville As String
+        Private _mville As String
 
-        Private codePostal As String
+        Private m_codePostal As String
 
-        Private courriel As String
+        Private m_courriel As String
 
-        Private isAdmin As Boolean
+        Private m_isAdmin As Boolean
 
-        Private arrierePlan As String
+        Private m_arrierePlan As String
 
-        Private collections As Dictionary(Of Collection.Type, Collection)
+        Private m_collections As Dictionary(Of Collection.Type, Collection)
 
         ''' <summary>
         ''' Constructeur par defaut.
         ''' </summary>
         Public Sub New()
-            prenom = "Homer"
-            nom = "Simpson"
-            nomUtilisateur = "hsimpson"
+            m_prenom = "Homer"
+            m_nom = "Simpson"
+            m_nomUtilisateur = "hsimpson"
         End Sub
 
         ''' <summary>
-        ''' Construit un membre avec son id dans la base de donnees
+        ''' Construit un membre avec son id dans la base de donnees.
+        ''' Le membre va se construire en chargeant ses attributs depuis la base de donnees passee en parametre.
+        ''' Si aucun membre n'a cet id, une exception est levee.
         ''' </summary>
-        Public Sub New(id As Integer)
-            Dim dbCon As MySqlConnection = New MySqlConnection("Server=G264-11;Database=test;Uid=root;Pwd=toor;")
+        Public Sub New(id As Integer, dbCon As MySqlConnection)
+            ' Verification de l'existence du membre en bdd
+            If Not existe(id, dbCon) Then
+                Throw New ApplicationException("Aucun membre n'a l'id " & id & "!")
+            End If
+            ' Execution de la requete SQL
             Dim dbCom As MySqlCommand = New MySqlCommand("SELECT * FROM membre WHERE idmembre=" & id, dbCon)
-            Dim dbRead As MySqlDataReader
-            dbCon.Open()
-            dbRead = dbCom.ExecuteReader()
+            Dim dbRead As MySqlDataReader = dbCom.ExecuteReader()
             dbRead.Read()
-            prenom = dbRead.GetString("prenom")
-            nom = dbRead.GetString("nom")
-            dbCon.Close()
+            ' Chargement des attributs
+            m_prenom = dbRead.GetString("prenom")
+            m_nom = dbRead.GetString("nom")
         End Sub
 
         ''' <summary>
-        ''' Verifie l'existence d'un membre avec un nom d'utilisateur
+        ''' Verifie l'existence d'un membre avec un id dans la base de donnees.
         ''' </summary>
-        ''' <returns>True si un utilisateur avec ce nom d'utilisateur existe, false sinon</returns>
-        Public Shared Function existe(nomUtilisateur As String) As Boolean
-            ' TODO 
-            Return False
+        ''' <returns>True si un utilisateur avec cet id existe, false sinon</returns>
+        Private Shared Function existe(id As Integer, dbCon As MySqlConnection) As Boolean
+            Dim dbCom As MySqlCommand = New MySqlCommand("SELECT COUNT(*) FROM membre WHERE idmembre=" & id, dbCon)
+            Dim nb As Integer = dbCom.ExecuteScalar()
+            Return (nb <> 0)
         End Function
+
+        ''' <summary>
+        ''' Accesseur du nom du membre
+        ''' </summary>
+        Public ReadOnly Property nom() As String
+            Get
+                Return m_nom
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Accesseur du prenom du membre
+        ''' </summary>
+        Public ReadOnly Property prenom() As String
+            Get
+                Return m_prenom
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Accesseur du nom d'utilisateur (pseudonyme) du membre
+        ''' </summary>
+        Public ReadOnly Property nomUtilisateur() As String
+            Get
+                Return m_nomUtilisateur
+            End Get
+        End Property
 
         ''' <summary>
         ''' Accesseur du nom complet (Prenom Nom) du membre
--- contenu/HobbyCartes/HobbyCartes/MembreEnvoiMessage.aspx.vb
+++ contenu/HobbyCartes/HobbyCartes/MembreEnvoiMessage.aspx.vb
@@ -1,15 +1,28 @@
-﻿Public Class MembreEnvoiMessage
+﻿Imports MySql.Data.MySqlClient
+
+Public Class MembreEnvoiMessage
     Inherits System.Web.UI.Page
+
+    Private dbCon As MySqlConnection
 
     Private membre As Entitees.Membre
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Protected Sub Page_Load() Handles Me.Load
+        ' Connexion a la base de donnees
+        dbCon = New MySqlConnection("Server=G264-11;Database=test;Uid=root;Pwd=toor;")
+        dbCon.Open()
+        ' Chargement du membre avec le parametre "id" de l'url
         Dim idMembre As Integer = Request.QueryString("id")
-        membre = New Entitees.Membre(idMembre)
+        membre = New Entitees.Membre(idMembre, dbCon)
+        ' Application du nom du membre sur le label "Nom"
         lbNom.Text = membre.nomComplet
     End Sub
 
     Protected Sub btnEnvoyer_Click() Handles btnEnvoyer.Click
 
+    End Sub
+
+    Protected Sub Page_Unload() Handles Me.Unload
+        dbCon.Close()
     End Sub
 End Class
