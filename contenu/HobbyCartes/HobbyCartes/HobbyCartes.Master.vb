cb0c97a75f6137496071c2667ab01d8d9951f1f3
Charles Levesque <dreamtooloverture@gmail.com>
2012-11-05 14:30:04 -0500

    Affichage du nom d'utilisateur lorsque connecté terminé

--- contenu/HobbyCartes/HobbyCartes/HobbyCartes.Master.vb
+++ contenu/HobbyCartes/HobbyCartes/HobbyCartes.Master.vb
@@ -1,12 +1,18 @@
-﻿Public Class HobbyCartes
+﻿Imports MySql.Data
+Imports MySql.Data.MySqlClient
+
+Public Class HobbyCartes
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim connection As MySqlConnection = New MySqlConnection(My.Resources.StringConnexionBdd)
+        connection.Open()
+
         mnuProfil.Visible = False
         mnuAdmin.Visible = False
         If Not Session("connected") Is Nothing Then
             If Boolean.Parse(Session("connected")) Then
-                lblConnecter.Text = "Connecté"
+                lblConnecter.Text = "Connecté en tant que " + New Entites.Membre(Integer.Parse(Session("idMembre")), connection).nomUtilisateur
                 lblConnecter.ForeColor = Drawing.Color.LightGreen
                 hlnkConnection.Visible = False
                 hlnkDeconnection.Visible = True
@@ -30,6 +36,8 @@
                 hlnkDeconnection.Visible = False
             End If
         End If
+
+        connection.Close()
     End Sub
 
     Protected Sub hlnkDeconnection_OnClick(sender As Object, e As EventArgs) Handles hlnkDeconnection.Click
