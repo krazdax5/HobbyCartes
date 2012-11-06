8214afbc8f2db5f951f5a12bcf761fe28749800a
Charles Levesque <dreamtooloverture@gmail.com>
2012-11-06 09:09:00 -0500

    UPDATE! Service web pour hobbycartes terminé

--- contenu/HobbyCartes/Lise/ISecurite_hc.vb
+++ contenu/HobbyCartes/Lise/ISecurite_hc.vb
@@ -1,6 +1,6 @@
 ﻿' REMARQUE : vous pouvez utiliser la commande Renommer du menu contextuel pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
 <ServiceContract()>
-Public Interface securite_hc
+Public Interface ISecurite_hc
 
     <OperationContract()>
     Function GetData(ByVal value As Integer) As String
@@ -17,7 +17,7 @@ Public Interface securite_hc
 
 End Interface
 
-' Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+' Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service
 
 <DataContract()>
 Public Class CompositeType
